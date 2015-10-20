bd299b69863198ed419aacbf7bcbec267b8ebcd0
glenndewildt <gwildt@avans.nl>
2015-10-20 16:14:33 +0200

    Bezig met het opzetten van db via POCO

---
+++ Kwisspel/Kwisspel/Model/Vraag.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kwisspel.Model
+{
+    [Table("Vraag")]
+   public  class Vraag
+    {
+        
+        public int Id { get; set; }
+
+        public string Soort { get; set; }
+
+        public int Antwoord { get; set; }
+
+        public string StringVraag { get; set; }
+
+    }
+}
---
+++ Kwisspel/Kwisspel/ViewModel/VragenVM.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kwisspel.ViewModel
+{
+    class VragenVM
+    {
+        private Model.Vraag vraag;
+        public int Id {
+            get {
+                return vraag.Id;
+            }
+            set {
+                vraag.Id = value;
+            }
+        }
+
+        public string Vraag
+        {
+            get
+            {
+                return vraag.StringVraag;
+            }
+            set
+            {
+                vraag.StringVraag = value;
+            }
+        }
+
+        public string Soort
+        {
+            get
+            {
+                return vraag.Soort;
+            }
+            set
+            {
+                vraag.Soort = value;
+            }
+        }
+
+         public VragenVM(Model.Vraag dier)
+        {
+            // TODO: Complete member initialization
+            this.vraag = vraag;
+        }
+
+        public VragenVM()
+        {
+            vraag = new Model.Vraag();
+        }
+
+        public Model.Vraag ToPOCO()
+        {
+            return vraag;
+        }
+
+    }
+}
