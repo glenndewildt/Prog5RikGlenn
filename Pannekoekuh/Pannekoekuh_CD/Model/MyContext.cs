22a4d98b7609d79ba2892dadc16c3d6209802e57
rscholte2 <rscholte2@avans.nl>
2015-10-20 20:25:39 +0200

    Pannekoek uitwerking toegevoegd

---
+++ Pannekoekuh/Pannekoekuh_CD/Model/MyContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pannekoekuh_CD.Model
+{
+    class MyContext : DbContext
+    {
+  
+
+        public DbSet<Pannekoek> Pannekoeken { get; set; }
+    }
+}
