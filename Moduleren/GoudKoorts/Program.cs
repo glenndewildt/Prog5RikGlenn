a35cc7c1dadd537714df07d2da0e1dc7720f68c1
GlenndeWildt <gwildt@avans.nl>
2015-10-29 20:01:57 +0100

    Model

---
+++ Moduleren/GoudKoorts/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoudKoorts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
