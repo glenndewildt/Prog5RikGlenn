6f0f5670bdbf28335beccf5a48d2b9f3dc714a3f
GlenndeWildt <gwildt@avans.nl>
2015-10-31 15:06:51 +0100

    Pff kutwerkje

--- Moduleren/GoudKoorts/Program.cs
+++ Moduleren/GoudKoorts/Program.cs
@@ -9,6 +9,7 @@ namespace GoudKoorts
 {
     class Program
     {
+
         static Board board = new Board();
         static void Main(string[] args)
         {
@@ -32,120 +33,158 @@ namespace GoudKoorts
 
         private static void timer_Tick(object sender, ElapsedEventArgs e)
         {
+         
             Console.Clear();
             Console.WriteLine();
             Console.WriteLine(board.Score);
             Console.WriteLine();
+            
             for (int i = 0; i < 35; i++)
             {
                 
                 Console.Write(board.schipChar[i]);
             }
+
+            int counter = 0;
             Console.WriteLine();
             foreach (var track in board.DockPath)
             {
-                if (track.ToChar() == 'C')
+                if (counter != 5)
                 {
-                    Console.Write(' ');
-                    
+                    if (track.GetType() == typeof(DevergingSwtich))
+                    {
+                        Console.Write(' ');
+
+                    }
+
+                    else if (track.GetType() == typeof(ConvergingSwitch))
+                    {
+
+                        Console.Write(' ');
+                    }
+                    else
+                    {
+                        Console.Write(track.ToChar());
+                    }
                 }
-           
-               
-                else if (track.ToChar() == 'I')
-                {
+                else {
                     Console.Write(' ');
-                    
+                }
+                    counter = counter +1;
+                }
+
+
+            counter = 0;
+            Console.WriteLine();
+            foreach (var track in board.DockPath)
+            {
+                if (counter != 5)
+                {
+                    if (track.GetType() == typeof(DevergingSwtich))
+                    {
+                        Console.Write(track.ToChar());
+
+                    }
+
+                    else if (track.GetType() == typeof(ConvergingSwitch))
+                    {
+                        Console.Write(track.ToChar());
+                    }
+                    else
+                    {
+                        Console.Write(' ');
+                    }
                 }
                 else {
                     Console.Write(track.ToChar());
-            }
-                
-            }
-            Console.WriteLine();
-            foreach (var track in board.DockPath)
-            {
-                if (track.ToChar() == 'C')
-                {
-                    Console.Write('C');
-
                 }
-
-
-                else if (track.ToChar() == 'I')
-                {
-                    Console.Write('I');
-
-                }
-                else
-                {
-                    Console.Write(' ');
-                }
+                counter++;
 
             }
+            counter = 0;
             Console.WriteLine();
             foreach (var track in board.SecondPath)
             {
-                if (track.GetType() == typeof(DevergingSwtich))
+                if (counter != 5 && counter != 10)
                 {
-                    Console.Write(' ');
+                    if (track.GetType() == typeof(DevergingSwtich))
+                    {
+                        Console.Write(' ');
 
-                }
+                    }
 
+                    else if (track.GetType() == typeof(ConvergingSwitch))
+                    {
 
-            
-
-                else if (track.GetType() == typeof(ConvergingSwitch)) {
-                    Console.Write(' ');
+                        Console.Write(' ');
+                    }
+                    else
+                    {
+                        Console.Write(track.ToChar());
+                    }
                 }
                 else
                 {
-                    Console.Write(track.ToChar());
+                    Console.Write(' ');
                 }
-            }
+                counter = counter + 1;
 
+
+            }
+            counter = 0;
             Console.WriteLine();
             foreach (var track in board.SavePath)
             {
-                if (track.ToChar() == 'C')
+                if (counter != 10)
                 {
-                    Console.Write('C');
+                    if (track.GetType() == typeof(DevergingSwtich))
+                    {
+                        Console.Write(track.ToChar());
+
+                    }
+
+                    else if (track.GetType() == typeof(ConvergingSwitch))
+                    {
+                        Console.Write(track.ToChar());
+                    }
+                    else
+                    {
+                        Console.Write(' ');
+                    }
+                }
+                else {
+                    Console.Write(track.ToChar());
 
                 }
-
-
-                else if (track.ToChar() == 'I')
-                {
-                    Console.Write('I');
-
-                }
-               
-                else
-                {
-                    Console.Write(' ');
-                }
-
+                counter++;
             }
-
+            counter = 0;
             Console.WriteLine();
             foreach (var track in board.SavePath)
             {
-                if (track.ToChar() == 'C')
+                if (counter != 10)
                 {
-                    Console.Write(' ');
+                    if (track.GetType() == typeof(DevergingSwtich))
+                    {
+                        Console.Write(' ');
 
+                    }
+
+                    else if (track.GetType() == typeof(ConvergingSwitch))
+                    {
+                        Console.Write(' ');
+                    }
+                    else
+                    {
+                        Console.Write(track.ToChar());
+                    }
                 }
-
-
-                else if (track.ToChar() == 'I')
-                {
-                    Console.Write(' ');
-
+                else { 
+                Console.Write(' ');
                 }
-                else
-                {
-                    Console.Write(track.ToChar());
-                }
+                counter++;
             }
+
             Console.WriteLine();
             board.Move();
            
